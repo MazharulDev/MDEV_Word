504c6c47e488eb5ee8e1791b9d531e56722fc8c4
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2023-01-28 09:30:07 +0600

    crate toolbar and undo option added

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ class MDEVWord(QMainWindow):
         super(MDEVWord, self).__init__()
         self.editor = QTextEdit()
         self.setCentralWidget(self.editor)
+        self.showMaximized()
+        self.editor.setFontPointSize(20)
+        self.create_tool_bar()
+
+    def create_tool_bar(self):
+        tool_bar = QToolBar()
+        undo_action = QAction('Undo', self)
+        undo_action.triggered.connect(self.editor.undo)
+        tool_bar.addAction(undo_action)
+
+        self.addToolBar(tool_bar)
 
 
 app = QApplication(sys.argv)
